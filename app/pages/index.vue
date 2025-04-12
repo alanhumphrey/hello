cb36e9b6e10cae9c114737b40390bbb37161b877
Alan Humphrey <alan@greymaple.org>
2025-04-12 11:31:19 -0700

    add text to index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,44 +5,46 @@ const color = useState('color', () => colors[Math.floor(Math.random() * colors.l
 </script>
 
 <template>
-  <div class="centered">
-    <h1 :style="{ color }">
-      {{ runtimeConfig.public.helloText }}
-    </h1>
-    <NuxtLink
-      to="/"
-      external
-    >
-      refresh
-    </NuxtLink>
-  </div>
+    <div class="centered">
+        <h1 :style="{ color }">
+            {{ runtimeConfig.public.helloText }}
+        </h1>
+        <p>Added text</p>
+        <NuxtLink to="/" external>
+            refresh
+        </NuxtLink>
+    </div>
 </template>
 
 <style scoped>
 .centered {
-  position: absolute;
-  width: 100%;
-  text-align: center;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  margin: 0;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    position: absolute;
+    width: 100%;
+    text-align: center;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    margin: 0;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
+
 h1 {
-  font-size: 32px;
+    font-size: 32px;
 }
+
 @media (min-width: 768px) {
-  h1 {
-    font-size: 64px;
-  }
+    h1 {
+        font-size: 64px;
+    }
 }
+
 a {
-  color: #888;
-  text-decoration: none;
-  font-size: 18px;
+    color: #888;
+    text-decoration: none;
+    font-size: 18px;
 }
+
 a:hover {
-  text-decoration: underline;
+    text-decoration: underline;
 }
 </style>
